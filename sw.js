b4f84bb7603d912f5d66b883752d588959a927ca
kokhinmaungwin <khinmaungwin675@gmail.com>
2025-11-11 14:46:29 +0630

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,7 +9,6 @@ self.addEventListener('install', event => {
         './',
         './index.html',
         './manifest.json',
-        './sw.js',
         './icons/icon-192.png',
         './icons/icon-512.png',
       ]);
